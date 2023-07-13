83632d1c16f98f8d67862f9c10183ec416eed518
as-o9l7 <131327246+as-o9l7@users.noreply.github.com>
2023-07-13 12:22:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Smooth Scrool Sample
+
+// slider option
+
+$(function(){
+   $('.bxslider').bxSlider({
+     mode: 'fade',
+     captions: true,
+     slideWidth: 400
+   });
+ });
+
+$(function(){
+
+   $('a[href^="#"]').click(function() {
+
+      // 初期設定：移動時間(ms)と頭出し位置
+      var speed = 500;
+      var offset = -0;
+
+      // アンカーを取得
+      var anchor = $(this).attr("href");
+
+      // ターゲットの位置を取得
+      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+      var position = target.offset().top + offset;
+
+      // スクロール（アニメーション）
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+      return false;
+
+   });
+
+});
+
